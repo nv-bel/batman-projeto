7e6a7454ec9babee4263af3f360733f9b2f973cb
Isabel Veloso <102189522+nv-bel@users.noreply.github.com>
2024-10-14 20:48:44 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,4 @@
-// const express = require('express');
-// const { authenticateUser } = require('./auth');  // Importa a função de autenticação
-
-// const app = express();
-
-// app.use(express.json());
-
-// // Rota de login
-// app.post('/login', (req, res) => {
-//   const { username, password } = req.body;
-//   const role = authenticateUser(username, password);
-  
-//   if (!role) {
-//     return res.status(401).json({ message: 'Usuário ou senha inválidos' });
-//   }
-
-//   res.json({ message: `Bem-vindo, ${username}! Você é um ${role}.` });
-// });
-
-// // Rota protegida de dashboard
-// app.get('/dashboard', (req, res) => {
-//   const { username, role } = req.query; // Obtendo os dados do usuário da query string
-
-//   if (!username || !role) {
-//     return res.status(401).json({ message: 'Acesso negado. Faça login primeiro.' });
-//   }
-
-//   if (role === 'admin') {
-//     return res.json({ message: 'Bem-vindo ao dashboard de administrador.' });
-//   } else if (role === 'gerente') {
-//     return res.json({ message: 'Bem-vindo ao dashboard de gerente.' });
-//   } else if (role === 'funcionario') {
-//     return res.json({ message: 'Bem-vindo ao dashboard de funcionário.' });
-//   } else {
-//     return res.status(403).json({ message: 'Sem acesso.' });
-//   }
-// });
-
-// app.listen(3000, () => {
-//   console.log('Servidor rodando na porta 3000');
-// });
-
-
-// Importando o Express
+ // Importando o Express
 const express = require('express');
 const path = require('path');
 
